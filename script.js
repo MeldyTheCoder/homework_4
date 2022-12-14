99c6d5f5d9dd999b19ca834edd8b62b283a9b956
Meldy <53063665+MeldyTheCoder@users.noreply.github.com>
2022-12-14 20:46:41 +0300

    v1

---
+++ script.js
@@ -0,0 +1,30 @@
+const student = "Иванов Иван Иванович"; // Очевидно, что здесь ваши личные Фамилия, Имя и Отчество
+
+document.getElementById("student").innerHTML = student;
+
+// Отсюда и ниже идет ваш код решения домашнего задания
+// ...
+
+const error_chance = 0.2;
+
+function throwRandomError(chance) {
+    let max_chance = 1
+    let min_chance = 0.0
+    if ((chance < min_chance) || (chance > max_chance)) {
+        throw new Error('Значение шанса должно быть в промежутке от 0.1 до 1');
+    }
+    
+    random_number = Math.random(max_chance).toFixed(1);
+    alert(`Случайное число: ${random_number}`);
+    if (random_number <= chance) {
+        throw new Error('Ой, что-то сломалось :(');
+    }
+}
+
+
+try {
+    throwRandomError(error_chance)
+    alert('Всё в порядке, приложение работает в штатном режиме')
+} catch (e) {
+    alert(`Произошла ошибка, пожалуйста перезагрузите страницу. Текст ошибки: ${e.message}`)
+}
